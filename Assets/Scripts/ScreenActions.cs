b1d525a2272069281adbf3b9810473e3c8f9a510
Al Saleh <alayham@alayham.com>
2017-03-03 14:53:38 +0100

    Created the Question class

--- Assets/Scripts/ScreenActions.cs
+++ Assets/Scripts/ScreenActions.cs
@@ -2,6 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+class Question
+{
+    string qText = "";
+    string qAnswer = "";
+    string uAnswer = "";
+
+    public Question (string QText, string QAnswer)
+    {
+        qText = QText;
+        qAnswer = QAnswer;
+    }
+    public string QText
+    {
+        get { return qText; }
+    }
+    public string UAnswer
+    {
+        get { return uAnswer; }
+        set { uAnswer = value; }
+    }
+    public bool IsCorrect()
+    {
+        return uAnswer == qAnswer;
+    }
+}
+
 public class ScreenActions : MonoBehaviour {
 
 	// Use this for initialization
